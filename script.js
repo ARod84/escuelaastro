d60530def759e28ea1374692a122859be4ef25c8
Araima Rodríguez <rodriguezaraima@gmail.com>
2022-03-26 23:41:34 -0300

    Script

--- script.js
+++ script.js
@@ -4,5 +4,20 @@
 *-----------------------------------*
 *************************************/
 // Variables interfaz
+
+
 // Variables globales
+
+let lunaVolumen = prompt("¿Cuál es el tamaño (volumen) de la Luna?");
+let lunaDistancia = prompt("¿Cuán lejos crees que esta la Luna de la Tierra?");
+let lunaGravedad = prompt("¿Cuánto vale la gravedad en la superficie de la Luna?");
+
+let solVolumen = prompt("¿Cuál es el tamaño (volumen) de la Sol?");
+let solDistancia = prompt("¿Cuán lejos crees que esta la Sol de la Tierra?");
+let solGravedad = prompt("¿Cuánto vale la gravedad en la superficie del Sol?");
+
+let marteVolumen = prompt("¿Cuál es el tamaño (volumen) de la Marte?");
+let marteDistancia = prompt("¿Cuán lejos crees que esta la Marte de la Tierra?");
+let marteGravedad = prompt("¿Cuánto vale la gravedad en la superficie de la Marte?");
+
 
