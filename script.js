564ae9fa89068bb79f78189b0ee880d6596879c7
Araima Rodríguez <rodriguezaraima@gmail.com>
2022-03-27 00:07:17 -0300

    Clases

--- script.js
+++ script.js
@@ -21,3 +21,52 @@ let marteDistancia = prompt("¿Cuán lejos crees que esta la Marte de la Tierra?
 let marteGravedad = prompt("¿Cuánto vale la gravedad en la superficie de la Marte?");
 
 
+// Clases objetos principales
+
+// Clases astros
+
+class Planeta {
+    constructor (volumen, distancia, gravedad) {
+        this.volumen = volumen;
+        this.distancia = distancia;
+        this.gravedad = gravedad;
+    }
+
+    validacion() {
+        if (this.volumen < 5000 && this.distancia < 6000 && this.gravedad < 7000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class Luna extends Planeta {
+    constructor(planeta) {
+        this.planeta = planeta;
+    }
+
+    validacion() {
+        if (this.volumen < 9000 && this.distancia < 10000 && this.gravedad < 11000 && this.planeta === "tierra") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class Estrella extends Planeta {
+    constructor (temperatura, nucleo, tipo) {
+        this.temperatura = temperatura;
+        this.nucleo = nucleo;
+        this.tipo = tipo;
+    }
+
+    validacion() {
+        if (this.volumen < 5000 && this.distancia < 6000 && this.gravedad < 7000 && this.tipo) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
